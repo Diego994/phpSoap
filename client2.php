0142518d47cb27593923f0161c394a4e6092c61a
diego <diegoguerrero994@gmail.com>
2020-06-09 01:04:37 -0500

    initial commit

--- client2.php
+++ client2.php
@@ -19,28 +19,61 @@
 
     $users = $cliente->call(
         "getById",
-        array('hostName' => $conexion->hostName,'dbName' => $conexion->dbName,'user' => $conexion->user,'password' => $conexion->password, 'table' => 'user', 'id' => 1),
+        array('hostName' => $conexion->hostName,'dbName' => $conexion->dbName,'user' => $conexion->user,'password' => $conexion->password, 'table' => 'user', 'id' => 2),
         "uri:$serverURL"
     );
 
     $users = json_decode($users);
-
-
-    $auth = $cliente->call(
-        "authenticate",
-        array('hostName' => $conexion->hostName,'dbName' => $conexion->dbName,'user' => $conexion->user,'password' => $conexion->password, 'table' => 'user', 'field' => 'email', 'passwordField' => 'password', 'fieldVal' => 'dguerrero@storecheck.com', 'passwordVal' => '12345678'),
-        "uri:$serverURL"
-    );
-
-    echo $auth;
-
-    
-
     echo "<ul>";
     foreach ($users as $user) {
         echo "<li>".$user->name."</li>";
-    }
+        echo "<li>".$user->lastName."</li>";
+        echo "<li>".$user->password."</li>";
+        echo "<li>".$user->email."</li>";
+    };
     echo "</ul>";
 
+    $email = $cliente->call(
+        "getByEmail",
+        array('hostName' => $conexion->hostName,'dbName' => $conexion->dbName,'user' => $conexion->user,'password' => $conexion->password, 'table' => 'user', 'email' => 'dguerrero@storecheck.com'),
+        "uri:$serverURL"
+    );
+
+    $auth = $cliente->call(
+        "authenticate",
+        array('hostName' => '192.168.100.18','dbName' => $conexion->dbName,'user' => $conexion->user,'password' => $conexion->password, 'table' => 'user', 'field' => 'email', 'passwordField' => 'password', 'fieldVal' => 'dguerrero@storecheck.com', 'passwordVal' => '12345678'),
+        "uri:$serverURL"
+    );
+
+
+
+
+    //echo $auth;
+
+    $arrayUser = json_decode($email, true);
+    echo $arrayUser[0]['id'];
+    
+
+    //print_r($regis);
+
+
+    /*$session = $cliente->call(
+        "getSessionById",
+        array('hostName' => $conexion->hostName,'dbName' => $conexion->dbName,'user' => $conexion->user,'password' => $conexion->password, 'id' => 1, 'idAdmin' => 1),
+        "uri:$serverURL"
+    );*/
+
+    $compra = $cliente->call(
+        "buyByUserEmail",
+        array('hostName' => $conexion->hostName,'dbName' => $conexion->dbName,'user' => $conexion->user,'password' => $conexion->password, 'table' => 'shopping', 'costColumn' => 'cost', 'idUserColumn' => 'idUser', 'idUser' => 17, 'userEmail' => 'diegoguerrero994@gmail.com', 'resultAlias' => 'compra'),
+        "uri:$serverURL"
+    );
+
+
+    $resultado = json_decode($compra, true);
+
+    echo $resultado[0]['compra'];
+
+    
     
 ?>
--- server2.php
+++ server2.php
@@ -26,6 +26,19 @@
         $miURL
     );
 
+    $server->register('getAllPurchasesByClientId',
+        array('hostName' => 'xsd:string',
+                'dbName' => 'xsd:string',
+                'user' => 'xsd:string',
+                'password' => 'xsd:string',
+                'table' => 'xsd:string',
+                'userIdColumn' => 'xsd:string',
+                'idUser' => 'xsd:int'
+            ),
+        array('return' => 'xsd:string'),
+        $miURL
+    ); 
+
     $server->register('getById',
         array('hostName' => 'xsd:string',
                 'dbName' => 'xsd:string',
@@ -65,6 +78,90 @@
         $miURL
     );  
 
+    $server->register('registerProduct',
+        array('hostName' => 'xsd:string',
+                'dbName' => 'xsd:string',
+                'user' => 'xsd:string',
+                'password' => 'xsd:string', 
+                'name' => 'xsd:string',
+                'cost' => 'xsd:int',
+                'description' => 'xsd:string',
+                'availability' => 'xsd:int',
+                'stock' => 'xsd:int',
+                'image' => 'xsd:string'
+            ),
+        array('return' => 'xsd:string'),
+        $miURL
+    );  
+
+    $server->register('getSessionById',
+        array('hostName' => 'xsd:string',
+                'dbName' => 'xsd:string',
+                'user' => 'xsd:string',
+                'password' => 'xsd:string', 
+                'id' => 'xsd:int',
+                'idAdmin' => 'xsd:int'
+            ),
+        array('return' => 'xsd:string'),
+        $miURL
+    );  
+
+    $server->register('deleteById',
+        array('hostName' => 'xsd:string',
+                'dbName' => 'xsd:string',
+                'user' => 'xsd:string',
+                'password' => 'xsd:string', 
+                'table' => 'xsd:string', 
+                'id' => 'xsd:int'
+            ),
+        array('return' => 'xsd:string'),
+        $miURL
+    );  
+
+    $server->register('deletePurchases',
+        array('hostName' => 'xsd:string',
+                'dbName' => 'xsd:string',
+                'user' => 'xsd:string',
+                'password' => 'xsd:string', 
+                'table' => 'xsd:string', 
+                'id' => 'xsd:int'
+            ),
+        array('return' => 'xsd:string'),
+        $miURL
+    );  
+        
+
+    $server->register('addPurchase',
+        array('hostName' => 'xsd:string',
+                'dbName' => 'xsd:string',
+                'user' => 'xsd:string',
+                'password' => 'xsd:string', 
+                'table' => 'xsd:string', 
+                'idUser' => 'xsd:int',
+                'idProduct' => 'xsd:int',
+                'productName' => 'xsd:string', 
+                'cost' => 'xsd:int',
+                'image' => 'xsd:string' 
+            ),
+        array('return' => 'xsd:string'),
+        $miURL
+    );  
+
+    $server->register('buyByUserEmail',
+        array('hostName' => 'xsd:string',
+                'dbName' => 'xsd:string',
+                'user' => 'xsd:string',
+                'password' => 'xsd:string', 
+                'table' => 'xsd:string', 
+                'costColumn' => 'xsd:string', 
+                'idUserColumn' => 'xsd:string', 
+                'idUser' => 'xsd:int',
+                'userEmail' => 'xsd:string',
+                'resultAlias' => 'xsd:string'
+            ),
+        array('return' => 'xsd:string'),
+        $miURL
+    );  
     
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -84,6 +181,16 @@
         return json_encode($data);
     }
 
+    function getAllPurchasesByClientId($hostName,  $dbName,  $user,  $password, $table, $userIdColumn, $idUser){
+        $cn = mysqli_connect($hostName,$user,$password,$dbName);
+        $query = $cn->query("SELECT * FROM ".$table." WHERE ".$userIdColumn."=".$idUser);
+        $data = [];
+        while ($row = mysqli_fetch_array($query,MYSQLI_ASSOC)) {
+            $data[] = $row ;
+        }
+        return json_encode($data);
+    }
+
     function getById($hostName,  $dbName,  $user,  $password, $table, $id){
         $cn = mysqli_connect($hostName,$user,$password,$dbName);
         $query = $cn->query("SELECT * FROM ".$table." WHERE id = ".$id);
@@ -94,9 +201,9 @@
         return json_encode($data);
     }
 
-    function getByEmail($hostName,  $dbName,  $user,  $password, $table, $id){
+    function getByEmail($hostName,  $dbName,  $user,  $password, $table, $email){
         $cn = mysqli_connect($hostName,$user,$password,$dbName);
-        $query = $cn->query("SELECT * FROM ".$table." WHERE email = ".$email);
+        $query = $cn->query("SELECT * FROM ".$table." WHERE email = '".$email."'");
         $data = [];
         while ($row = mysqli_fetch_array($query,MYSQLI_ASSOC)) {
             $data[] = $row ;
@@ -104,11 +211,33 @@
         return json_encode($data);
     }
 
+    function buyByUserEmail($hostName,  $dbName,  $user,  $password, $table, $costColumn, $idUserColumn, $idUser, $userEmail, $resultAlias){
+        $cn = mysqli_connect($hostName,$user,$password,$dbName);
+        $query = $cn->query("SELECT sum(".$costColumn.") as ".$resultAlias." from ".$table." where ".$idUserColumn."=".$idUser);
+        $data = [];
+        while ($row = mysqli_fetch_array($query,MYSQLI_ASSOC)) {
+            $data[] = $row ;
+        }
+        $data = json_encode($data);
+        $resultado = json_decode($data, true);
+
+        $total = $resultado[0]['compra'];
+
+        $to      = $userEmail;
+        $subject = 'recibo de compra';
+        $message = 'TeniStore le hace llegar su monto a pagar; un total de: '.$total.' pesos';
+        $headers = 'From: tenistore@store.com' . "\r\n" .
+            'Reply-To: tenistore@store.com' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+        mail($to, $subject, $message, $headers);
+    }
+
     function authenticate($hostName,  $dbName,  $user,  $password, $table, $field, $passwordField,$fieldVal,$passwordVal){
         $cn = mysqli_connect($hostName,$user,$password,$dbName);
-        $queryField = $cn->query("SELECT ".$field." FROM ".$table." WHERE ".$field." = ".$fieldVal);
+        $queryField = $cn->query("SELECT ".$field." FROM ".$table." WHERE ".$field." = '".$fieldVal."'");
         $cn2 = mysqli_connect($hostName,$user,$password,$dbName);
-        $queryPass = $cn->query("SELECT ".$passwordField." FROM ".$table." WHERE ".$field." = ".$fieldVal);
+        $queryPass = $cn->query("SELECT ".$passwordField." FROM ".$table." WHERE ".$field." = '".$fieldVal."'");
 
 
         $data = '';
@@ -138,6 +267,58 @@
             return 'false';
         }
     }
+
+    function registerProduct($hostName,  $dbName,  $user,  $password, $name, $cost ,$description, $availability, $stock, $image){
+        $cn = mysqli_connect($hostName,$user,$password,$dbName);
+        $query = $cn->query("INSERT INTO product(name,cost,description,availability,stock,image) VALUES('$name',$cost,'$description',$availability,$stock,'$image');");
+        mysqli_fetch_array($query,MYSQLI_ASSOC);
+        return 'true';
+    }
+
+    function getSessionById($hostName,  $dbName,  $user,  $password, $id, $idAdmin){
+        if($id == $idAdmin){
+            return 'true';
+        } else {
+            return 'false';
+        }
+    }
+
+    function deleteById($hostName,  $dbName,  $user,  $password, $table, $id){
+        $cn = mysqli_connect($hostName,$user,$password,$dbName);
+        $query = $cn->query("DELETE FROM ".$table." WHERE id = ".$id);
+        mysqli_fetch_array($query,MYSQLI_ASSOC);
+        return 'true';
+    }
+
+    function deletePurchases($hostName,  $dbName,  $user,  $password, $table, $id){
+        $cn = mysqli_connect($hostName,$user,$password,$dbName);
+        $query = $cn->query("DELETE FROM ".$table." WHERE idUser = ".$id);
+        mysqli_fetch_array($query,MYSQLI_ASSOC);
+        return 'true';
+    }
+
+    function addPurchase($hostName,  $dbName,  $user,  $password, $table, $idUser, $idProduct, $productName, $cost, $image){
+        $conn = new mysqli($hostName,$user,$password,$dbName);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO ".$table." (idUser,idProduct,name,cost,image) VALUES ('$idUser','$idProduct','$productName','$cost','$image')";
+        
+        if ($conn->query($sql) === TRUE) {
+            return "New record created successfully";
+        } else {
+          return "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+          $conn->close();
+    }
+        
+        /*$data = [];
+        while ($row = mysqli_fetch_array($query,MYSQLI_ASSOC)) {
+            $data[] = $row ;
+        }
+        return json_encode($data);*/
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
